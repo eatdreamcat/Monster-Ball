8c61505ada599ecc04e4e757900eca09cab9a8a3
Vincy Wang <vincy@celer.network>
2019-08-09 17:46:24 +0800

    v2.7

--- main.5063e.js
+++ main.5063e.js
@@ -138,7 +138,7 @@ window.boot = function () {
         BK.Script.loadlib();
     }
     else {
-        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.e53b4.js';
+        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.e8adf.js';
         if (jsList) {
             jsList = jsList.map(function (x) {
                 return 'src/' + x;
@@ -174,7 +174,7 @@ window.boot = function () {
     cc.game.run(option, onStart);
 };
 
-// main.5ae92.js is qqplay and jsb platform entry file, so we must leave platform init code here
+// main.5063e.js is qqplay and jsb platform entry file, so we must leave platform init code here
 if (false) {
     BK.Script.loadlib('GameRes://src/settings.js');
     BK.Script.loadlib();
